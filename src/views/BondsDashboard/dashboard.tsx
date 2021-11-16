f59fc82f9b6a7569e056820987c0cee3fd09ba29
Ignacio Freijomil <94308111+ignacio-eth@users.noreply.github.com>
2021-11-15 22:58:24 -0300

    Bonding Pools Dashboard and QOL

--- src/views/BondsDashboard/dashboard.tsx
+++ src/views/BondsDashboard/dashboard.tsx
@@ -14,15 +14,13 @@ import CakeWalletBalance from 'views/Home/components/CakeWalletBalance'
 import CardValue from 'views/Home/components/CardValue'
 import DashboardPage from 'components/layout/DashboardPage'
 import farms from 'state/farms'
-import { FaTelegramPlane, FaTwitter, FaDiscord, FaChartBar, FaMoneyBillAlt, FaTractor, FaHome, FaPrescriptionBottleAlt, FaTumblrSquare, FaCode, FaFlask, FaBook, FaReddit, FaRocketchat, FaRocket, FaBroadcastTower, FaLayerGroup, FaSeedling, FaExclamationTriangle, FaBootstrap, FaLandmark, FaGamepad, FaCircle, FaParachuteBox, FaVoteYea, FaProjectDiagram, FaShieldAlt, FaFire, FaCloud, FaPlayCircle, FaClipboard, FaUser, FaTwitterSquare, FaEnvelopeOpenText, FaDochub, FaHistory, FaHandHolding, FaHandHoldingUsd, FaQuestion, FaQuestionCircle } from 'react-icons/fa';
-
+import { FaTelegramPlane, FaTwitter, FaDiscord, FaChartBar, FaMoneyBillAlt, FaTractor, FaHome, FaPrescriptionBottleAlt, FaTumblrSquare, FaCode, FaFlask, FaBook, FaReddit, FaRocketchat, FaRocket, FaBroadcastTower, FaLayerGroup, FaSeedling, FaExclamationTriangle, FaBootstrap, FaLandmark, FaGamepad, FaCircle, FaParachuteBox, FaVoteYea, FaProjectDiagram, FaShieldAlt, FaFire, FaCloud, FaPlayCircle, FaClipboard, FaUser, FaTwitterSquare, FaEnvelopeOpenText, FaDochub, FaHistory, FaHandHolding, FaHandHoldingUsd, FaQuestion, FaQuestionCircle, FaMonument } from 'react-icons/fa';
 import { useTotalRewards } from 'hooks/useTickets'
-
-import useTokenBalance, { useTotalSupply, useBurnedBalance } from 'hooks/useTokenBalance'
+import useTokenBalance, { useTotalSupply, useBurnedBalance, useUstTreasuryBalance, useRvrsOneTreasuryBalance, useRvrsUstTreasuryBalance } from 'hooks/useTokenBalance'
 import { Link } from 'react-router-dom'
 import ExpandableSectionButton from 'components/ExpandableSectionButton'
 import Container from 'components/layout/Container'
-import { getCakeAddress } from '../../utils/addressHelpers'
+import { getCakeAddress, getRvrsOneAddress, getRvrsUstAddress, getUstAddress } from '../../utils/addressHelpers'
 
 const Price = styled.button`
   -webkit-box-align: center;
@@ -79,10 +77,7 @@ const StyledBtn = styled.button`
   max-width: 40px;
   padding: 12px;
   box-shadow: 0px 0px 10px #ccc;
-
-  `
-
-
+`
 
 const Test = styled.text`
   background-color: rgba(0, 0, 0,0) !important;
@@ -90,7 +85,30 @@ const Test = styled.text`
   font-size: 20px;
   font-weight: 600;
   text-shadow: 0px 0px 10px #ccc;
+`
 
+const Divider = styled.div`
+  background-color: #1E2129;
+  height: 0px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 7px;
+  margin-bottom: 7px;
+  width: 0%;
+`
+
+const ExpandingWrapper = styled.div<{ expanded: boolean }>`
+  height: ${(props) => (props.expanded ? '100%' : '0px')};
+  overflow: hidden;
+`
+const DetailsCard = styled.div`
+  background: rgba(0, 0, 0,0) !important;
+  border-radius: 0px;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 10px;
+  position: center;
+  text-align: center;
 `
 
 const Stat = styled.text`
@@ -111,22 +129,17 @@ const DCard = styled.div`
   padding: 30px;
   box-shadow: 1px 1px 3px #fff;
   background-image: linear-gradient(to right, #3B4557 , #2B3344);
-
-
-
   position: center;
   text-align: center;
 `
 
 const Sub = styled.p`
-color: #7F8997;
-font-size: 13px;
-font-weight: 500;
-margin-bottom: 0px;
-text-shadow: 0px 0px 0px #4E5C6D;
+  color: #7F8997;
+  font-size: 13px;
+  font-weight: 500;
+  margin-bottom: 0px;
+  text-shadow: 0px 0px 0px #4E5C6D;
 `
-
-
 const DashboardCard = styled.div`
   align-self: baseline;
   background-image: linear-gradient(to right, #2E3646, #3B4557 , #2B3344);
@@ -142,7 +155,6 @@ const DashboardCard = styled.div`
 
   border: 0px solid #fff;
   box-shadow: 0px 0px 20px #ccc;
-
 `
 
 const DashCard = styled.div`
@@ -155,12 +167,7 @@ const DashCard = styled.div`
   padding: 15px;
   position: relative;
   text-align: center;
-
-
-
 `
-
-  
 const MoneyWheel: React.FC = () => {
   const [pendingTx, setPendingTx] = useState(false)
   const { account } = useWallet()
@@ -168,32 +175,22 @@ const MoneyWheel: React.FC = () => {
   const farmsWithBalance = useFarmsWithBalance()
   const cakeBalance = getBalanceNumber(useTokenBalance(getCakeAddress()))
   const eggPrice = usePriceCakeBusd().toNumber()
-
   const totalSupply = useTotalSupply()
   const burnedBalance = useBurnedBalance(getCakeAddress())
-
   const [showExpandableSection, setShowExpandableSection] = useState(false)
-
-
-
   const earningsSum = farmsWithBalance.reduce((accum, farm) => {
     return accum + new BigNumber(farm.balance).div(new BigNumber(10).pow(18)).toNumber()
   }, 0)
   const balancesWithValue = farmsWithBalance.filter((balanceType) => balanceType.balance.toNumber() > 0)
-
   const { onReward } = useAllHarvest(balancesWithValue.map((farmWithBalance) => farmWithBalance.pid))
-
   const cakePriceUsd = usePriceCakeBusd()
   const misPrice = usePriceCakeBusd();
   const tokenPrice = cakePriceUsd.toNumber().toFixed(2);
-
   const circSupply = totalSupply ? totalSupply.minus(burnedBalance) : new BigNumber(0);
   const cakeSupply = getBalanceNumber(circSupply);
   const circulatingMath = new BigNumber(cakeSupply).minus(5000000);
   const circulatingRVRS = circulatingMath.toNumber().toFixed(0);
-
   const mCap = misPrice.times(circulatingRVRS).toNumber().toFixed(0);
-
   const marketCap = ( misPrice.times(circSupply).isNaN() || ! misPrice.times(circSupply).isFinite() 
   ? new BigNumber(0) : misPrice.times(circSupply) );
 
@@ -202,6 +199,21 @@ const MoneyWheel: React.FC = () => {
   if(farms && farms[0] && farms[0].vikingPerBlock){
     vikingPerBlock = new BigNumber(farms[0].vikingPerBlock).div(new BigNumber(10).pow(18)).toNumber();
   }
+
+  // Treasury Balances
+  const ustTreasuryBalance = getBalanceNumber(useUstTreasuryBalance(getUstAddress()))
+
+  const rvrsOneTreasuryBalance = getBalanceNumber(useRvrsOneTreasuryBalance(getRvrsOneAddress()))
+  const rvrsUstTreasuryBalance = getBalanceNumber(useRvrsUstTreasuryBalance(getRvrsUstAddress()))
+
+  const RvrsOneValue = new BigNumber(rvrsOneTreasuryBalance).times(2.8)
+  const RvrsUstValue = new BigNumber(rvrsUstTreasuryBalance).times(5.65)
+
+  const RvrsOneValueString = RvrsOneValue.toNumber().toLocaleString('en-us',{ maximumFractionDigits: 1 })
+  const RvrsUstValueString = RvrsUstValue.toNumber().toLocaleString('en-us',{ maximumFractionDigits: 1 })
+  const ustTreasuryBalanceString = ustTreasuryBalance.toLocaleString('en-us',{ maximumFractionDigits: 1 })
+
+  const treasuryBalance = new BigNumber(RvrsOneValue).plus(RvrsUstValue).plus(ustTreasuryBalance).plus(612000).plus(372800).toNumber().toLocaleString('en-us',{ maximumFractionDigits: 0 })
 
   const harvestAllFarms = useCallback(async () => {
     setPendingTx(true)
@@ -217,101 +229,74 @@ const MoneyWheel: React.FC = () => {
 
   return (
     <DashboardPage2>
-
       <DashboardCard>
         <div>
+          <DashCard style={{padding:'40px', marginLeft:'0px', marginRight:'0px'}}>
 
-            <DashCard style={{padding:'40px', marginLeft:'0px', marginRight:'0px'}}>
+            <Flex justifyContent='space-between' alignItems='left'  ml='10px' mr='10px'  marginBottom='-5px' mt='0px'> 
+              <Flex>
+                <Test>{TranslateString(9299, 'Reverseum Treasury')}</Test>
+              </Flex>
+              <ExpandableSectionButton onClick={() => setShowExpandableSection(!showExpandableSection)}/>
+            </Flex>
 
-              <Flex justifyContent='space-between' alignItems='left'  ml='10px' mr='10px'  marginBottom='-5px' mt='0px'> 
-
-                <Flex>
-                  <Test>{TranslateString(9299, 'Reverseum Treasury')}</Test>
-                    
-                </Flex>
-
-                  <ExpandableSectionButton onClick={() => setShowExpandableSection(!showExpandableSection)}/>
-
-                </Flex>
-
-              <ExpandingWrapper expanded={showExpandableSection}>
+            <ExpandingWrapper expanded={showExpandableSection}>
               <DetailsCard>
-
-
-
-                <Flex justifyContent="left" mt="30px"  marginBottom='15px' >
-                  <Stat>Total Market Value: $NaN</Stat>
+                <Flex justifyContent="left" mt="30px"  marginBottom='8px' >
+                  <Stat><FaMonument/>Estimated Treasury Value</Stat>
                 </Flex>
+                
+                  <Flex justifyContent="left" mt="1px"  marginBottom='8px' marginLeft='5px'>
+                    <Sub>~${treasuryBalance}</Sub>
+                  </Flex>
 
                 <Flex justifyContent="left" mt="10px"  marginBottom='8px' >
-                  <Stat><FaChartBar/> Distribution</Stat>
+                  <Stat><FaChartBar/> Reverseum Distribution</Stat>
                 </Flex>
 
-                <Flex justifyContent="left" mt="1px"  marginBottom='8px' >
-                  <Sub>- %NaN</Sub>
-                </Flex>
+                  <Flex justifyContent="left" mt="1px"  marginBottom='8px' marginLeft='5px'>
+                    <Sub>~{ustTreasuryBalanceString} UST in Treasury</Sub>
+                  </Flex>
 
-                <Flex justifyContent="left" mt="1px"  marginBottom='8px' >
-                  <Sub>- %NaN</Sub>
-                </Flex>
+                  <Flex justifyContent="left" mt="1px"  marginBottom='8px' marginLeft='5px'>
+                    <Sub>~${RvrsOneValueString} In RVRS-ONE</Sub>
+                  </Flex>
 
-                <Flex justifyContent="left" mt="1px"  marginBottom='8px' >
-                  <Sub>- %NaN</Sub>
-                </Flex>
+                  <Flex justifyContent="left" mt="1px"  marginBottom='8px' marginLeft='5px'>
+                    <Sub>~${RvrsUstValueString} In RVRS-UST</Sub>
+                  </Flex>
 
                 <Flex justifyContent="left" mt="10px"  marginBottom='8px' >
-                  <Stat><FaProjectDiagram/> Current Strategies</Stat>
+                  <Stat><FaProjectDiagram/> Strategies</Stat>
                 </Flex>
 
-                <Flex justifyContent="left" mt="1px"  marginBottom='8px' >
-                  <Sub>- %NaN</Sub>
-                </Flex>
+                  <Flex justifyContent="left" mt="1px"  marginBottom='8px' marginLeft='5px' >
+                    <Sub>~$112,000, Sushiswap (138% APY)</Sub>
+                  </Flex>
 
-                <Flex justifyContent="left" mt="1px"  marginBottom='8px' >
-                  <Sub>- %NaN</Sub>
-                </Flex>
+                  <Flex justifyContent="left" mt="1px"  marginBottom='8px' marginLeft='5px'>
+                    <Sub>~$400,000, Mirror/Spectrum (56% APY) </Sub>
+                  </Flex>
 
-                <Flex justifyContent="left" mt="1px"  marginBottom='8px' >
-                  <Sub>- %NaN</Sub>
-                </Flex>
+                  <Flex justifyContent="left" mt="1px"  marginBottom='8px' marginLeft='5px'>
+                    <Sub>~$372,800, Anchor (20% APY) </Sub>
+                  </Flex>
+
+                  <Flex justifyContent="left" mt="1px"  marginBottom='8px' marginLeft='5px'>
+                    <Sub>~$100,000, Ethereum (0% APY) </Sub>
+                  </Flex>
 
                 <Flex justifyContent="left" mt="10px"  marginBottom='8px' >
-                  <Stat><FaHistory/> Updated Every 24hs*</Stat>
+                  <Stat><FaHistory/> Strategies Balance Updated Every ~48hs*</Stat>
                 </Flex>
-
-
-                </DetailsCard>
-
-              </ExpandingWrapper>
-            </DashCard>
-          </div>
-        </DashboardCard>
-    </DashboardPage2>
+              </DetailsCard>
+            </ExpandingWrapper>
+          </DashCard>
+        </div>
+      </DashboardCard>
+  </DashboardPage2>
   )
 }
 export default MoneyWheel
 
 
-const Divider = styled.div`
-  background-color: #1E2129;
-  height: 0px;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 7px;
-  margin-bottom: 7px;
-  width: 0%;
-`
-
-const ExpandingWrapper = styled.div<{ expanded: boolean }>`
-  height: ${(props) => (props.expanded ? '100%' : '0px')};
-  overflow: hidden;
-`
-const DetailsCard = styled.div`
-background: rgba(0, 0, 0,0) !important;
-border-radius: 0px;
-  flex-direction: column;
-  justify-content: space-around;
-  padding: 10px;
-  position: center;
-  text-align: center;
-`
